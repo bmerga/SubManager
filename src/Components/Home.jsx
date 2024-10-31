c939a2319e2699ede0efe3394fc4e0b74eed5380
bmerga <bekedire@gmail.com>
2024-10-31 23:03:57 +0300

    second page adding icons

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import '../App.css'
+import { Container, Grid2, Paper, styled, Typography, Box} from '@mui/material'
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+import SignalCellularAltOutlinedIcon from '@mui/icons-material/SignalCellularAltOutlined';
+const StyledPaper = styled(Paper)(({ theme }) => ({
+   padding: theme.spacing(1), 
+   backgroundColor: theme.palette.background.paper, 
+   boxShadow: theme.shadows[3], 
+}));
+
 function Home() {
   return (
     <div className='container'>
@@ -15,14 +25,68 @@ function Home() {
              
               <p className='no-credit-card'> No credit card required. Free 14-day trial</p>
            </div>
-           <div className='active-subscrption'>
-              <div className='active-subscription-month'> <p>Active subscrption</p> <p>$84.00/month</p></div> 
-              <div className='Netflix'> <button className='Netflix-button'>Netflix Monthly</button>$12.00</div>
-              <div className='Spotify'> <button className='Spotify-button'>Spotify Monthly</button>$9.99</div>
-              <div className='Amazon-prime'> <button className='Amazon-prime-button'>Amazon Prime Monthly</button>$139</div>
-           </div>
-        </div>
+           <Container>
             
+               <Box display="flex" gap={28} >
+              <Typography variant="h5" paddingTop={4} paddingBottom={5}>Active Subscrption</Typography>
+              <Typography variant="h5" paddingTop={4}>$84.00/month</Typography>
+           </Box>
+           <StyledPaper>
+            <Box display="flex" gap={33} paddingLeft={10}>
+              <Typography variant="h5" >Netflix Monthly</Typography>
+              <Typography variant="h5">$12.00</Typography>
+           </Box>
+           </StyledPaper> <br></br>
+           <StyledPaper>
+            <Box display="flex" gap={33} paddingLeft={10}>
+              <Typography variant="h5">Spotify Monthly</Typography>
+              <Typography variant="h5">$9.99</Typography>
+           </Box>
+           </StyledPaper> <br></br>
+           <StyledPaper>
+            <Box display="flex" gap={20} paddingLeft={10}>
+              <Typography variant="h5">Amazon Prime Annual</Typography>
+              <Typography variant="h5">$139.00</Typography>
+           </Box>
+           </StyledPaper>    
+              
+         </Container>
+        </div>
+           
+            <p className='wcop'>Why Choose Our Platform</p> 
+            <p className='eyn'>Everything you need to manage subscriptions effectively</p>
+          
+           
+           <Container>
+            <div style={{display:"flex", gap:"4%", padding:"5% 1%", }}>
+            <StyledPaper>
+               <Box  >
+                     {/* <Button> {<NotificationsOutlinedIcon />}</Button> */}
+                     <Typography variant="h4">Analytics Dashboard</Typography>
+                     <Typography variant="h5">Get detailed insights into your 
+                     spending patterns and subscription usage.</Typography>
+                              
+              </Box>
+            </StyledPaper> 
+            <StyledPaper>
+               <Box  >
+                  <Typography variant="h4">Smart Reminders</Typography>
+                  <Typography variant="h5">Never miss a payment with  
+                   automated notifications and renewal reminders.</Typography>
+              </Box>
+            </StyledPaper>
+            <StyledPaper>
+               <Box  >
+                  <Typography variant="h4">Cost Optimazation</Typography>
+                  <Typography variant="h5">Get recomendations to reduce costs  
+                   and identify unused subscriptions.</Typography>
+              </Box>
+            </StyledPaper>
+            </div>
+             
+           
+      </Container>
+    
     </div>
   )
 }
